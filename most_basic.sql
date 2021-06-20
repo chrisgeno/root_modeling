024d3064b0a1d1839a21cb697677e4f8d0760494
Chris Geno <chris@chrisgeno.net>
2021-06-20 17:27:59 -0500

    Delete most_basic.sql

--- most_basic.sql
+++
@@ -1,6 +0,0 @@
-select d.id as driver_id,
-       t.id as trip_id,
-       datediff(minute, started_at, ended_at) as trip_duration,
-       t.distance
-from driver d
-left join trips t on d.id = t.driver_id
